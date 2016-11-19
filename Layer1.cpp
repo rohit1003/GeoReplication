55adfff88857889dcfc82ee669520473a4977d87
rtiwari2 <rtiwari2@illinois.edu>
2016-11-18 23:52:41 -0600

    Variation 3

--- Layer1.cpp
+++ Layer1.cpp
@@ -1,6 +1,4 @@
 #include "Headers.h"
-
-#define MAXBUFLEN 10000
 vector<string> splitString(string input, string delimiter)
   {
      vector<string> output;
@@ -22,9 +20,79 @@ vector<string> splitString(string input, string delimiter)
      return output;
 
  }
+mutex mtx; 
 unordered_map<int,int> m;
 
+class ClientSocket{
+			public:
+			int sockfd;
+            /* connector’s address information */
+			struct sockaddr_in their_addr;
+			struct hostent *he;
+			int numbytes,addr_len;
+		    char buffer[10000];
 
+		    ClientSocket(int Port,string ip){
+		    	string ip ="localhost";
+				if ((he = gethostbyname(ip.c_str())) == NULL) {
+					cout<<"Client-gethostbyname() error"<<endl;;
+					exit(1);
+				}
+				else {	
+						cout<<"Client-gethostname() is OK"<<endl;
+				}
+				if((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) == -1) {
+					cout<<"Client-socket() error lol!"<<endl;
+					exit(1);
+				}
+				else {
+					cout<<"Client-socket() sockfd is OK..."<<endl;			/* host byte order */
+				}
+
+				their_addr.sin_family = AF_INET;							/* short, network byte order */
+				addr_len = sizeof(struct sockaddr);
+				cout<<"Using port: "<<Port<<endl;
+				their_addr.sin_port = htons(Port);
+				their_addr.sin_addr = *((struct in_addr *)he->h_addr);       /* zero the rest of the struct */
+				memset(&(their_addr.sin_zero), '\0', 8);
+
+			}
+			string send_request(string str){
+
+				strcpy(buffer, str.c_str());
+				printf("\nsending --> %s",buffer);
+				if((numbytes = sendto(sockfd, buffer, strlen(buffer), 0, 
+				 	(struct sockaddr *)&their_addr, sizeof(struct sockaddr))) == -1) {
+					cout<<"Client-sendto() error !"<<endl;
+					exit(1);
+				}
+				else {
+					numbytes=recvfrom(sockfd, buffer, strlen(buffer), 0, 
+					(struct sockaddr *)&their_addr, (socklen_t*)&addr_len);
+
+					string result="";	    		
+		    		buffer[numbytes] = '\0';
+					result=result+string(buffer);
+
+					cout<<"Response is..."<<result<<endl;
+					cout<<"sent "<<numbytes<<" bytes to "<<inet_ntoa(their_addr.sin_addr)<<endl;
+					return result;
+				}
+				return NULL;
+
+			}
+			~ClientSocket(){
+		
+			/* Closing UDP socket */
+
+				if (close(sockfd) != 0)
+					cout<<"Client-sockfd closing is failed!"<<endl;
+				else
+					cout<<"Client-sockfd successfully closed!"<<endl;
+			}
+
+
+};
 
 class ServerSocket{
 	public:
@@ -35,10 +103,10 @@ class ServerSocket{
 	int addr_len, numbytes;
 	char buf[10000];
 	
-	ServerSocket(){
+	ServerSocket(int port_no){
 		numbytes=0;
 		my_addr.sin_family = AF_INET;			/* short, network byte order */
-		my_addr.sin_port = htons(4001);			/* automatically fill with my IP */
+		my_addr.sin_port = htons(port_no);			/* automatically fill with my IP */
 		my_addr.sin_addr.s_addr = INADDR_ANY;	/* zero the rest of the struct */
 		memset(&(my_addr.sin_zero), '\0', 8);
 
@@ -71,25 +139,46 @@ class ServerSocket{
 				result=result+string(buf);
 				
 				vector<string> v=splitString(result,"#");
-				cout<<"\nReceiving Request...";
+				cout<<"\nReceiving Request..."<<result;
 				
-				string response="";
-				if(v[0]=="RD"){
-				
-					cout<<"\nL1:The Read value is:"<<m[stoi(v[1])];
-					response = response + to_string(m[stoi(v[1])]);			
-				
-				} else if (v[0]=="WR"){
-				
-					m[stoi(v[1])] = stoi(v[2]);
-					cout<<"\nL1:The Written value is:"<<m[stoi(v[1])];
-				
-				} else {
+				//int _Lead_Port_DC1 = (stoi(v[1]) %2==0)?5200:6200;
+				int _Lead_Port_DC1 = 5200;
+				int _Lead_Port_DC2 = 6200;
 
+				/* Even-numbered requests would go to DC1 and odd-numbered to DC2*/
+				
+//				string _ip_DC1 = config_read((stoi(v[1]) %2==0)? "DC1_LEAD_IP" : "DC2_LEAD_IP");
+				string _ip_DC1 = config_read("DC1_LEAD_IP");
+				string _ip_DC2 = config_read("DC2_LEAD_IP");
+
+				ClientSocket c1(_Lead_Port_DC1,_ip_DC1);
+				ClientSocket c2(_Lead_Port_DC2,_ip_DC2);
+
+				string response_DC1="";
+				string response_DC2="";
+
+				if(v[0]=="RD"){
+
+					/* Send Synchronous writes to Leaders in all the DataCenters */
+					response_DC1 = response_DC1 + c1.send_request(result);		
+					response_DC2 = response_DC2 + c2.send_request(result);		
+					response_DC1 = response_DC1 + response_DC2;
+
+					cout<<"\nL1:The Read value is:"<<response_DC1;
+					/* Actually Forward the request to Leader */
+					/*response = response + to_string(m[stoi(v[1])]);*/
+				} else if (v[0]=="WR"){
+					response_DC1 = response_DC1 + c1.send_request(result);		
+					response_DC2 = response_DC2 + c2.send_request(result);		
+					response_DC1 = response_DC1 + response_DC2;
+
+					/*m[stoi(v[1])] = stoi(v[2]);*/
+					cout<<"\nL1:The Written value is:"<<response_DC1;
+				} else {
 					/* Drop the Request */
 				}
 
-				strcpy(buf, response.c_str());
+				strcpy(buf, response_DC1.c_str());
 				numbytes = sendto(sockfd,buf, strlen(buf), 0,(struct sockaddr *)&their_addr,sizeof(struct sockaddr));
        				if (numbytes  < 0) cout<<"\nFailed to send";
 			}
@@ -108,31 +197,30 @@ class ServerSocket{
 
 
 void th1_func(){
-	ServerSocket s;
+	string port = config_read("LAYER1_PORT")
+	ServerSocket s(stoi(port));
 
-	cout<<"\nThread 1 Spawned..";
+	cout<<"\nL1: Thread 1 Spawned..";
 	s.rec_request();		
 }
 int main(){
-	
 	/* Start Work Load Generation */
-	
 	thread th1(th1_func);
 	th1.join();
-
-	// system("rm workload");
-	// system("g++ -std=c++11 WorkLoadGen.cpp -o workload");
-	// 	if(system("./workload"))			{
-	// 	 Successful spawn of process 
-	// } else {
-	// 	//cout<<"WorkLoadGen start failed !! Aborting run";
-	// 	//exit(0);
-	// }
-	
-    // DC1 Port - 3000
-    // DC2 Port - 3001
     
-    /* */
+/*	system("rm workload");
+	system("g++ -std=c++11 WorkLoadGen.cpp -o workload");
+
+	if(system("./workload")){
+		 cout<<"Successful spawn of process ";
+	} else {
+		cout<<"WorkLoadGen start failed !! Aborting run";
+	}
+	
+    DC1 Port - 5200
+    DC2 Port - 6201
+    
+ */
 
 	return 0;
 }
